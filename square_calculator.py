85dfe72064e04e969a6199649673dea369902b39
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2023-11-21 19:18:23 +0200

    Square_calculator.py

--- square_calculator.py
+++ square_calculator.py
@@ -1,15 +1,5 @@
 import math
 
-def square_number(a):
-    sqrt_value = math.sqrt(a)
-
-    return f'{float(sqrt_value):.2f}'
-
-def square_number_1(b):
-    sqrt_value_1 = math.sqrt(b)
-
-    return f'{float(sqrt_value_1):.2f}'
-
 def square_result(a, b):
 
     return f'{math.sqrt(a) + math.sqrt(b):.2f}'
