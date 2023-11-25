f862f1c079add85cda5f28f615fe46f24481ca98
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2023-11-25 21:40:29 +0200

    square_calculator.py

--- square_calculator.py
+++ square_calculator.py
@@ -1,10 +1,40 @@
 import math
 
-def square_result(a, b):
+def square_number(a):
+    sqrt_value = math.sqrt(a)
+
+    return f'{float(sqrt_value):.2f}'
+
+def square_number_1(b):
+    sqrt_value_1 = math.sqrt(b)
+
+    return f'{float(sqrt_value_1):.2f}'
+
+def square_add_result(a, b):
 
     return f'{math.sqrt(a) + math.sqrt(b):.2f}'
 
-num = int(input())
-num1 = int(input())
+def square_subtract_result(a, b):
+    return f'{math.sqrt(a) - math.sqrt(b):.2f}'
 
-print(square_result(num, num1))
+def square_multiply_result(a, b):
+    return f'{math.sqrt(a) * math.sqrt(b):.2f}'
+
+def square_divide_result (a, b):
+    return f'{math.sqrt(a) / math.sqrt(b):.2f}'
+
+num1 = int(input())
+action = input()
+num2 = int(input())
+
+if action == '+':
+    print(square_add_result(num1, num2))
+
+if action == '-':
+    print(square_subtract_result(num1, num2))
+
+if action == '*':
+    print(square_multiply_result(num1, num2))
+
+if action == '/':
+    print(square_divide_result(num1,num2))
