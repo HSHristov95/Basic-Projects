2f1a365f5f4ada9b959ad5460e852d319c69bd13
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2023-11-27 22:15:48 +0200

    Add files via upload
    This is a Vat calculator which calculaes VAT in Bulgaria.

--- VAT_calculator.py
+++ VAT_calculator.py
@@ -1,14 +1,10 @@
-while True:
+select_type_unit = input('Select service or merch: ')
+
+while select_type_unit != 'end':
     services = []
-    select_type_unit = input('Select service or merch: ')
 
     if select_type_unit == 'service':
         service_name = input('Enter service name: ')
         services.append(service_name)
-        
-        if service_name == 'end':
-            break
-
-        service_name = input('Enter service name: ')
 
 print(services)
