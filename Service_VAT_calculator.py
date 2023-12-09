1e70b5c03679aca469490c30126bb2e50802c5d7
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2023-12-09 22:20:41 +0200

    Add files via upload
    This is vat calculator for services

---
+++ Service_VAT_calculator.py
@@ -0,0 +1,13 @@
+while True:
+    service_name = input('Enter service name: ')
+
+    if service_name == 'end':
+        break
+
+    start_location = input('Enter start location: ')
+    end_location = input('Enter end location: ')
+    service_price = float(input('Enter price of service: '))
+    vat_price = service_price * 0.20
+    total_price = service_price + vat_price
+
+print(f'{total_price:.2f}')
