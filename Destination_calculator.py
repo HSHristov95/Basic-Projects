91a4f385686defaa461302798187db4988560f04
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2023-12-09 21:56:33 +0200

    Add files via upload
    This is a calculator that prints time needed for traveling.

---
+++ Destination_calculator.py
@@ -0,0 +1,5 @@
+destination = int(input('Enter destination: '))
+speed = int(input('Enter travel speed: '))
+time = float(destination / speed)
+
+print(f'You will need {time:.2f} hours to reach destination.')
